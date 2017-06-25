8d7a49142f8643ee725719772e3fd937f6f81b35
Tom Br <Tom.bruyninx@gmail.com>
2017-06-25 22:49:46 +0200

    Factory Patterns for repositories

--- main.js
+++ main.js
@@ -5,13 +5,17 @@
 var Task = require('./task');
 var User = require('./user');
 var Project = require('./project');
-var taskRepo = require('./taskRepo');
-var userRepo = require('./userRepo');
-var projectRepo = require('./projectRepo');
+var repo = require('./repo');
 
 console.log("Task Applications \n");
 
-var task1 = new Task(taskRepo.get(1));
-console.log(task1.complete());
+var task1 = new Task(repo.task.get(1));
+var project1 = new Project(repo.project.get(1));
+var user1 = new User(repo.user.get(1));
+
+task1.linkUser(user1);
+task1.linkProject(project1);
+
+console.log(task1.project);
 
 
